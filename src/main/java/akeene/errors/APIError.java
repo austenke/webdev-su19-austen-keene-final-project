9648267392c82789565fb4acbae2a2273da46c4c
Austen Keene <austenkeene@gmail.com>
2019-06-22 23:42:08 -0400

    Add error handlers

---
+++ src/main/java/akeene/errors/APIError.java
@@ -0,0 +1,25 @@
+package akeene.errors;
+
+import com.auth0.jwt.internal.com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Arrays;
+
+public class APIError {
+    private final String[] errors;
+
+    @JsonCreator
+    public APIError(@JsonProperty("errors") final String... errors) {
+        this.errors = clone(errors);
+    }
+
+    @JsonProperty
+    public String[] getErrors() {
+        return clone(errors);
+    }
+
+    private static String[] clone(final String[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+}
